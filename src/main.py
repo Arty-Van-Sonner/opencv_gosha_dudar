d97f8b3ef556bc8f87706c0451ca344f2e64dc51
Arty-Van-Sonner <valter.199669@gmail.com>
2026-06-05 22:08:02 +0300

    Start with videos and added requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import cv2
 
-img = cv2.imread('data/images/image.png')
-cv2.imshow('Result', img)
-cv2.waitKey(0)
+# img = cv2.imread('data/images/image.png')
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
+
+
